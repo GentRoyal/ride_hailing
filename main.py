7e3eece995e7d77b8b39d7c7b5c25dfba06a1304
Ridwan Yusuf <ridwan.yusuf@whiteshield.com>
2026-06-05 04:38:05 +0100

    Update connections

--- main.py
+++ main.py
@@ -34,11 +34,12 @@ load_dotenv()
 # ---------------------------------------------------------------------------
 def get_conn():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST"),
         port=int(os.getenv("DB_PORT", 5432)),
-        dbname=os.getenv("DB_NAME", "ride_hailing_db"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", ""),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        sslmode="require"
     )
 
 
